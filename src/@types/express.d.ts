a68a204c9d402063057074926b4e4e9bc767ef86
fullsnacker <fullsnacker@gmail.com>
2025-05-15 14:49:14 -0300

    Fix types

--- src/@types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthCredentials } from '../../../core/validators/auth.validators';
-
-declare global {
-  namespace Express {
-    interface Request {
-      validatedData?: AuthCredentials;
-    }
-  }
-}
