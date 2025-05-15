b10f3e0e1670df073ff246c54b94957b22437f86
fullsnacker <fullsnacker@gmail.com>
2025-05-15 14:57:36 -0300

    Refactor for presentation layer

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import { UserFileRepository } from "./infrastructure/repositories/user.file.repository";
 import { AuthUseCase } from "./core/usecases/auth.usecase";
-import { AuthCLI } from "./infrastructure/cli/auth.cli";
 import { startServer } from "./infrastructure/http/server";
+import { AuthCLI } from "./presentation/cli/auth.cli";
 
-if (process.argv.includes('--api')) {
+if (process.argv.includes("--api")) {
   startServer(3000);
 } else {
+  const userRepository = new UserFileRepository();
+  const authUseCase = new AuthUseCase(userRepository);
+  const authCLI = new AuthCLI(authUseCase);
 
-const userRepository = new UserFileRepository();
-const authUseCase = new AuthUseCase(userRepository);
-const authCLI = new AuthCLI(authUseCase);
-
-authCLI.start().catch((error) => {
-  console.error("Application error:", error);
-  process.exit(1);
-});
+  authCLI.start().catch((error) => {
+    console.error("Application error:", error);
+    process.exit(1);
+  });
 }
